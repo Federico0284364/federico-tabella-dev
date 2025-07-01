b9ad7bb6ee058438ad23725ef8e5d8c44d716c2c
Federico0284364 <tabella.federico@gmail.com>
2025-07-01 09:30:09 +0200

    aggiunto bordi project

--- components/Project.tsx
+++ components/Project.tsx
@@ -19,7 +19,7 @@ export default function ProjectContainer({ project }: Props) {
 			href={project.link}
 			target="_blank"
 			rel="noopener noreferrer"
-			className="hover:bg-light transition p-4 md:p-6 rounded-2xl grid grid-cols-1 md:grid-cols-2 items-center gap-x-8"
+			className="backdrop-blur-lg bg-medium border-1 md:border-0 border-fg/20 hover:bg-light transition p-4 md:p-6 rounded-2xl grid grid-cols-1 md:grid-cols-2 items-center gap-x-8"
 		>
 			<Image
 				className="rounded-xl aspect[16/9]"
